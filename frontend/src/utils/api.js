24086fca7c139e51c25c4a449e13ce0a957507bc
Divyanshu Pal <86774540+divyanshu-pal@users.noreply.github.com>
2024-09-03 19:55:43 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://task-2-backend-1.onrender.com/api",
+  baseURL: "http://localhost:4000/api",
 });
 
 export default api;
